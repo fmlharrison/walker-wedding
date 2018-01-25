098381def00b12bb655c2f5cb12b8f1bd6631c77
Felix Harrison <fmlharrison@gmail.com>
2018-01-25 23:08:04 +0000

    Added fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,33 @@
 import React, { Component } from "react";
 
 import "../assets/css/header.css";
+import Title from "./Title";
 
-const links = [
-  "Our Story",
-  "Our Family",
-  "Our Firends",
-  "Lucy & Joe",
-  "The Big Day",
-  "The Gifts",
-  "RSVP"
-];
+const left_links = ["Our Story", "Our Family", "Our Firends"];
+
+const right_links = ["The Big Day", "The Gifts", "RSVP"];
 
 class Header extends Component {
   renderHeaderLinks = headerLinks => {
     return headerLinks.map(link => {
       return (
         <li>
-          <a className="header-link" href="#">{link}</a>
+          <a className="header-link" href="#">
+            {link}
+          </a>
         </li>
       );
     });
   };
 
   render() {
-    return <header className="header">{this.renderHeaderLinks(links)}</header>;
+    return (
+      <header className="header">
+        {this.renderHeaderLinks(left_links)}
+        <Title />
+        {this.renderHeaderLinks(right_links)}
+      </header>
+    );
   }
 }
 
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "../assets/css/title.css";
+
+class Title extends Component {
+  render() {
+    return <div className="title">Joe &amp; Lucy</div>;
+  }
+}
+
+export default Title;
