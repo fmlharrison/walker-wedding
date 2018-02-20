c97c01237d8f5dd4db0859d8e923ec7c351b9f73
Felix Harrison <fmlharrison@gmail.com>
2018-02-20 09:19:07 +0000

    Working nav bar when using mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,16 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-import "../assets/css/nav-bar.css"
+import "../assets/css/nav-bar.css";
 
 class NavBar extends Component {
   constructor(props) {
@@ -11,37 +20,57 @@ class NavBar extends Component {
     };
   }
 
-  toggleNavbar= () => {
+  toggleNavbar = () => {
     this.setState({
       collapsed: !this.state.collapsed
     });
-  }
+  };
+
+  closeNav = () => {
+    this.setState({ collapsed: true });
+  };
 
   render() {
     return (
       <div className="nav-bar-small">
         <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">WW</NavbarBrand>
+          <NavbarBrand href="#" className="mr-auto">
+            <Link className="nav-bar-small__title" to="/">
+              The Walker Wedding
+            </Link>
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
-              <NavItem>
-                <NavLink href="/our-story">Our Story</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/our-story">
+                  Our Story
+                </Link>
               </NavItem>
-              <NavItem>
-                <NavLink href="/our-family">Our Family</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/our-family">
+                  Our Family
+                </Link>
               </NavItem>
-              <NavItem>
-                <NavLink href="/our-friends">Our Friends</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/our-friends">
+                  Our Friends
+                </Link>
               </NavItem>
-              <NavItem>
-                <NavLink href="/the-big-day">The Big Day</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/the-big-day">
+                  The Big Day
+                </Link>
               </NavItem>
-              <NavItem>
-                <NavLink href="/the-gifts">The Gifts</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/the-gifts">
+                  The Gifts
+                </Link>
               </NavItem>
-              <NavItem>
-                <NavLink href="/rsvp">RSVP</NavLink>
+              <NavItem onClick={this.closeNav}>
+                <Link className="nav-bar-small__link" to="/rsvp">
+                  RSVP
+                </Link>
               </NavItem>
             </Nav>
           </Collapse>
